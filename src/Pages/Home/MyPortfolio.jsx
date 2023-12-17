cd2c605815c9c5a39636cba35cf3d8721f04afe5
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-12-18 07:26:36 +0800

    link git hub

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -1,8 +1,13 @@
+// src/Pages/Home/MyPortfolio.jsx
+
+// Import data dari file index.json di dalam direktori "../../data/"
 import data from "../../data/index.json";
 
+// Mendefinisikan fungsi MyPortfolio yang diekspor sebagai default
 export default function MyPortfolio() {
-  const githubLink = "https://github.com/Suyono-Sukorame"; // Tautan GitHub Anda
+  const githubLink = "https://github.com/Suyono-Sukorame"; // Menyimpan tautan GitHub Anda
 
+  // Render section HTML untuk menampilkan portfolio
   return (
     <section className="portfolio--section" id="MyPortfolio">
       <div className="portfolio--container-box">
@@ -11,14 +16,10 @@ export default function MyPortfolio() {
           <h2 className="section--heading">My Portfolio</h2>
         </div>
         <div>
-          <a href={githubLink} className="btn btn-github"> {/* Tambahkan tautan ke GitHub */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 33 33"
-              fill="none"
-            >
+          {/* Tautan menuju GitHub Anda */}
+          <a href={githubLink} className="btn btn-github">
+            {""}
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 33 33" fill="none">
               {/* ... (path SVG lainnya) */}
             </svg>
             Visit My GitHub
@@ -26,9 +27,11 @@ export default function MyPortfolio() {
         </div>
       </div>
       <div className="portfolio--section--container">
+        {/* Mapping data dari portfolio untuk menampilkan kartu-kartu proyek */}
         {data?.portfolio?.map((item, index) => (
           <div key={index} className="portfolio--section--card">
             <div className="portfolio--section--img">
+              {/* Menampilkan gambar proyek */}
               <img src={item.src} alt="Placeholder" />
             </div>
             <div className="portfolio--section--card--content">
@@ -37,15 +40,11 @@ export default function MyPortfolio() {
                 <p className="text-md">{item.description}</p>
               </div>
               <p className="text-sm portfolio--link">
-                <a href={item.link} target="_blank" rel="noopener noreferrer"> {/* Tautan proyek */}
-                  {item.link}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 20 19"
-                    fill="none"
-                  >
+                {/* Tautan eksternal menuju proyek */}
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  {" "}
+                  {item.link} {/* Menampilkan tautan proyek */}
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 19" fill="none">
                     {/* ... (path SVG lainnya) */}
                   </svg>
                 </a>
--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -1,18 +1,29 @@
+// src/Pages/Home/MySkills.jsx
+
+// Import React dan useState dari React library
 import React, { useState } from "react";
+
+// Import data dari file index.json di dalam direktori "../../data/"
 import data from "../../data/index.json";
 
+// Deklarasi komponen bernama MySkills
 const MySkills = () => {
+  // Menginisialisasi state expandedSkills dengan useState untuk menyimpan indeks skill yang diperluas
   const [expandedSkills, setExpandedSkills] = useState([]);
 
-  // Function to toggle the expanded state of a skill
+  // Fungsi toggleExpand untuk mengubah status perluasan suatu skill berdasarkan indeksnya
   const toggleExpand = (index) => {
+    // Memeriksa apakah indeks skill sudah ada dalam expandedSkills
     if (expandedSkills.includes(index)) {
+      // Jika ada, menghapus indeks skill tersebut dari expandedSkills
       setExpandedSkills(expandedSkills.filter((item) => item !== index));
     } else {
+      // Jika tidak ada, menambahkan indeks skill tersebut ke dalam expandedSkills
       setExpandedSkills([...expandedSkills, index]);
     }
   };
 
+  // Render section HTML untuk menampilkan skills
   return (
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
@@ -20,23 +31,21 @@ const MySkills = () => {
         <h2 className="skills--section--heading">My Expertise</h2>
       </div>
       <div className="skills--section--container">
+        {/* Mapping data dari skills dan menampilkan kartu-kartu skill */}
         {data?.skills?.map((item, index) => (
           <div key={index} className="skills--section--card">
             <div className="skills--section--img">
+              {/* Menampilkan gambar skill */}
               <img src={item.src} alt="Product Chain" />
             </div>
             <div className="skills--section--card--content">
               <h3 className="skills--section--title">{item.title}</h3>
-              {/* Display shortened or full description based on expanded state */}
+              {/* Menampilkan deskripsi yang disingkat atau lengkap berdasarkan status expandedSkills */}
               <p className="skills--section--description">
-                {expandedSkills.includes(index)
-                  ? item.description
-                  : `${item.description.slice(0, 100)}...`}
+                {expandedSkills.includes(index) ? item.description : `${item.description.slice(0, 100)}...`}
                 {item.description.length > 100 && (
-                  <button
-                    className="read-more-btn"
-                    onClick={() => toggleExpand(index)}
-                  >
+                  // Tombol untuk menampilkan lebih sedikit atau lebih banyak deskripsi skill
+                  <button className="read-more-btn" onClick={() => toggleExpand(index)}>
                     {expandedSkills.includes(index) ? "Show Less" : "Read More"}
                   </button>
                 )}
@@ -49,6 +58,5 @@ const MySkills = () => {
   );
 };
 
+// Export komponen MySkills agar bisa digunakan di file lain
 export default MySkills;
-
-
